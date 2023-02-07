3486c59539250616ba9e2934985f93c659b45df4
FerQue <fernando_quezada1785@outlook.com>
2023-02-06 20:21:09 -0500

    update 1

---
+++ SlotMachineApp/SlotMachineApp/SlotMachineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineAppApp.swift
+//  SlotMachineApp
+//
+//  Created by Victor Quezada on 2023-02-06.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
